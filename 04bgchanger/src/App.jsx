ee8ebbdce5ac63019685e40ad32bc2b9126df8a6
BarnalaManognaram <bmanognaram@gmail.com>
2024-10-18 11:34:11 +0530

    04bgchanger commit1

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -4,16 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [color, setColor] = useState('olive')
+  const [color, setColor] = useState('Red')
 
     
   return (
     <div className='w-screen h-screen duration-200 ' style={{backgroundColor: color}}>
       <div className='button-container'>
-      <button className='button' onClick={()=>setColor("Red")}>Red</button>
-      <button className='button'onClick={()=>setColor("green")}>green</button>
-      <button className='button' onClick={()=>setColor("yellow")}>yellow</button>
-      <button className='button'onClick={()=>setColor("white")}>white</button>
+      <button className='button bg-red-600' onClick={()=>setColor("Red")}>Red</button>
+      <button className='button bg-green-600'onClick={()=>setColor("green")}>green</button>
+      <button className='button bg-yellow-600' onClick={()=>setColor("yellow")}>yellow</button>
+      <button className='button bg-blue-600'onClick={()=>setColor("Blue")}>Blue</button>
       </div>
       
     </div>
